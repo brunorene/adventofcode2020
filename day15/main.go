379ae80790cf7a989c8e0e562ce2aba727a30ae2
Bruno Santos <brunorene@gmail.com>
2020-12-15 07:11:30 +0000

    day 15

---
+++ day15/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "log"
+
+func main() {
+
+	start := []int{9, 12, 1, 4, 17, 0, 18}
+	// start := []int{0, 3, 6}
+
+	part1(start, 2020)
+	part1(start, 30000000)
+}
+
+func part1(start []int, end int) {
+
+	memory := make(map[int][]int)
+
+	var last int
+	for now := 0; now < end; now++ {
+		if now < len(start) {
+			memory[start[now]] = []int{now}
+			last = start[now]
+		} else {
+			// new
+			if len(memory[last]) == 1 {
+				last = 0
+				_, exists := memory[0]
+				if !exists {
+					memory[0] = []int{}
+				}
+				memory[0] = append(memory[0], now)
+			} else {
+				last = memory[last][len(memory[last])-1] - memory[last][len(memory[last])-2]
+				memory[last] = append(memory[last], now)
+			}
+		}
+	}
+	log.Println(last)
+}
