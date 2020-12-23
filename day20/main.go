ecc5113a970937e0fd15e28312436f055e16983a
Bruno Santos <brunorene@gmail.com>
2020-12-23 07:47:00 +0000

    day 20 new approach WIP

--- day20/main.go
+++ day20/main.go
@@ -9,20 +9,6 @@ import (
 	"strings"
 )
 
-type sideMatch struct {
-	position string
-	rotation int
-	flip     int
-}
-
-type tile struct {
-	id       int64
-	rotation int
-	flip     int
-	square   [][]byte
-	sides    map[int64]sideMatch
-}
-
 func main() {
 	path, _ := os.Getwd()
 	file, err := os.Open(path + "/day20/input.txt")
@@ -33,35 +19,75 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	state := "tile"
-	var current tile
 	var lineIndex int
-	tiles := make(map[int64]tile)
+	tiles := make(map[int64][][]byte)
+	var currentID int64
 	for scanner.Scan() {
 		line := scanner.Text()
 		switch state {
 		case "tile":
-			id, _ := strconv.ParseInt(strings.Replace(strings.Replace(line, "Tile ", "", 1), ":", "", 1), 10, 64)
-			current = tile{id: id, sides: make(map[int64]sideMatch), square: [][]byte{}}
+			currentID, _ = strconv.ParseInt(strings.Replace(strings.Replace(line, "Tile ", "", 1), ":", "", 1), 10, 64)
+			tiles[currentID] = [][]byte{}
 			state = "line"
 			lineIndex = 0
 		case "line":
-			current.square = append(current.square, []byte(line))
+			tiles[currentID] = append(tiles[currentID], []byte(line))
 			lineIndex++
 			if lineIndex == 10 {
 				state = ""
 			}
 		default:
-			tiles[current.id] = current
 			state = "tile"
 		}
 	}
-	tiles[current.id] = current
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	part2(part1(tiles))
+	part1(tiles)
+}
+
+func view(id int64, tile [][]byte) {
+	fmt.Println(id)
+	for _, row := range tile {
+		for _, col := range row {
+			fmt.Print(string(col))
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func rotate(tile [][]byte) [][]byte {
+	result := [][]byte{}
+	for range tile {
+		result = append(result, make([]byte, len(tile)))
+	}
+	n := len(tile)
+	x := n / 2
+	y := n - 1
+	for i := 0; i < x; i++ {
+		for j := i; j < y-i; j++ {
+			result[i][j] = tile[y-j][i]
+			result[y-j][i] = tile[y-i][y-j]
+			result[y-i][y-j] = tile[j][y-i]
+			result[j][y-i] = tile[i][j]
+		}
+	}
+	return result
+}
+
+func copyTile(tile [][]byte) [][]byte {
+	result := [][]byte{}
+	for _, row := range tile {
+		copyRow := []byte{}
+		for _, item := range row {
+			copyRow = append(copyRow, item)
+		}
+		result = append(result, copyRow)
+	}
+	return result
 }
 
 func reverse(b []byte) []byte {
@@ -72,23 +98,31 @@ func reverse(b []byte) []byte {
 	return newB
 }
 
-func flipVert(t tile) tile {
-	result := tile{id: t.id, rotation: t.rotation, sides: make(map[int64]sideMatch)}
-	for _, line := range t.square {
-		result.square = append(result.square, reverse(line))
+func flipVert(tile [][]byte) [][]byte {
+	result := [][]byte{}
+	for _, line := range tile {
+		result = append(result, reverse(line))
 	}
 	return result
 }
 
-func flipHorz(t tile) tile {
-	result := rotate(t)
+func multipleRotate(tile [][]byte, times int) [][]byte {
+	result := copyTile(tile)
+	for i := 1; i <= times; i++ {
+		result = rotate(result)
+	}
+	return result
+}
+
+func flipHorz(tile [][]byte) [][]byte {
+	result := rotate(tile)
 	result = flipVert(result)
 	return multipleRotate(result, 3)
 }
 
-func multipleFlip(t tile, times int) tile {
+func multipleFlip(tile [][]byte, times int) [][]byte {
 	times = times % 4
-	result := copyTile(t)
+	result := copyTile(tile)
 	switch times {
 	case 1:
 		result = flipVert(result)
@@ -98,186 +132,107 @@ func multipleFlip(t tile, times int) tile {
 		result = flipVert(result)
 		result = flipHorz(result)
 	}
-	result.flip = times
 	return result
 }
 
-func rotate(t tile) tile {
-	result := tile{id: t.id, rotation: (t.rotation + 1) % 4, sides: make(map[int64]sideMatch)}
-	for range t.square {
-		result.square = append(result.square, make([]byte, len(t.square)))
-	}
-	n := len(t.square)
-	x := n / 2
-	y := n - 1
-	for i := 0; i < x; i++ {
-		for j := i; j < y-i; j++ {
-			result.square[i][j] = t.square[y-j][i]
-			result.square[y-j][i] = t.square[y-i][y-j]
-			result.square[y-i][y-j] = t.square[j][y-i]
-			result.square[j][y-i] = t.square[i][j]
-		}
-	}
-	return result
-}
-
-func multipleRotate(t tile, times int) tile {
-	result := copyTile(t)
-	if times > 0 {
-		result = rotate(t)
-	}
-	for i := 1; i < times; i++ {
-		result = rotate(result)
-	}
-	return result
-}
-
-func match(center tile, side tile) string {
+func match(center [][]byte, side [][]byte) string {
 	directions := make(map[string]int)
-	for pos := range center.square {
-		if center.square[0][pos] == side.square[9][pos] {
+	for pos := range center {
+		if center[0][pos] == side[9][pos] {
 			directions["top"]++
 		}
-		if center.square[9][pos] == side.square[0][pos] {
+		if center[9][pos] == side[0][pos] {
 			directions["bottom"]++
 		}
-		if center.square[pos][0] == side.square[pos][9] {
+		if center[pos][0] == side[pos][9] {
 			directions["left"]++
 		}
-		if center.square[pos][9] == side.square[pos][0] {
+		if center[pos][9] == side[pos][0] {
 			directions["right"]++
 		}
 	}
 	for direction, count := range directions {
-		if count == len(center.square) {
+		if count == len(center) {
 			return direction
 		}
 	}
 	return ""
 }
 
-func view(t tile) {
-	fmt.Println(t.id, t.rotation, t.flip, t.sides)
-	for _, row := range t.square {
-		for _, col := range row {
-			fmt.Print(string(col))
-		}
-		fmt.Println()
-	}
-	fmt.Println()
+type coordinates struct {
+	x, y int
 }
 
-func findSides(t tile, other map[int64]tile) tile {
-	for _, side := range other {
-		for r := 0; r < 4; r++ {
-			for f := 0; f < 4; f++ {
-				changed := multipleRotate(side, r)
-				changed = multipleFlip(changed, f)
-				position := match(t, changed)
-				if position != "" {
-					t.sides[changed.id] = sideMatch{position, changed.rotation, changed.flip}
+func move(c coordinates, direction string) coordinates {
+	newC := c
+	switch direction {
+	case "top":
+		newC.y = c.y - 1
+	case "bottom":
+		newC.y = c.y + 1
+	case "left":
+		newC.x = c.y - 1
+	case "right":
+		newC.x = c.y + 1
+	}
+	return newC
+}
+
+func part1(tiles map[int64][][]byte) {
+	picture := make(map[coordinates][][]byte)
+	ids := make(map[coordinates]int64)
+	coords := make(map[int64]coordinates)
+	tilesToProcess := []int64{}
+	for k, pic := range tiles {
+		tilesToProcess = append(tilesToProcess, k)
+		ids[coordinates{0, 0}] = k
+		picture[coordinates{0, 0}] = pic
+		coords[k] = coordinates{0, 0}
+		break
+	}
+	for {
+		fmt.Println(tilesToProcess)
+		fmt.Println(ids)
+		fmt.Println(coords)
+		mainID := tilesToProcess[0]
+		tilesToProcess = tilesToProcess[1:]
+		_, exists := coords[mainID]
+		if exists {
+
+		}
+		for id, tile := range tiles {
+			if id != mainID {
+				direction := ""
+				for r := 0; r < 4; r++ {
+					for f := 0; f < 4; f++ {
+						result := multipleRotate(tile, r)
+						result = multipleFlip(result, f)
+						direction = match(tiles[mainID], result)
+						if direction != "" {
+							tileCoords := move(coords[mainID], direction)
+							tilesToProcess = append(tilesToProcess, id)
+							ids[tileCoords] = id
+							picture[tileCoords] = result
+							coords[id] = tileCoords
+							break
+						}
+					}
+					if direction != "" {
+						break
+					}
+				}
+				_, existsTop := ids[move(coords[mainID], "top")]
+				_, existsBottom := ids[move(coords[mainID], "bottom")]
+				_, existsLeft := ids[move(coords[mainID], "left")]
+				_, existsRight := ids[move(coords[mainID], "right")]
+				if existsBottom && existsTop && existsRight && existsLeft {
 					break
 				}
 			}
 		}
-	}
-	return t
-}
-
-func copyMap(src map[int64]tile) map[int64]tile {
-	dst := make(map[int64]tile)
-	for k, v := range src {
-		dst[k] = v
-	}
-	return dst
-}
-
-func copyTile(t tile) tile {
-	result := tile{t.id, t.rotation, t.flip, [][]byte{}, make(map[int64]sideMatch)}
-	for _, row := range t.square {
-		copyRow := []byte{}
-		for _, item := range row {
-			copyRow = append(copyRow, item)
-		}
-		result.square = append(result.square, copyRow)
-	}
-	for k, v := range t.sides {
-		result.sides[k] = v
-	}
-	return result
-}
-
-func remove(m map[int64]tile, key int64) map[int64]tile {
-	result := copyMap(m)
-	delete(result, key)
-	return result
-}
-
-func part1(tiles map[int64]tile) map[int64]tile {
-	mul := int64(1)
-	for _, main := range tiles {
-		other := remove(tiles, main.id)
-		main = findSides(main, other)
-		if len(main.sides) == 2 {
-			mul *= main.id
+		if len(tilesToProcess) == 0 {
+			break
 		}
 	}
-	fmt.Println(mul)
-	return tiles
-}
-
-func removeBorder(t tile) [][]byte {
-	result := [][]byte{}
-	for i := 1; i < len(t.square)-1; i++ {
-		result = append(result, t.square[i][1:len(t.square)-1])
-	}
-	return result
-}
-
-func nextTile(t tile, position string, tiles map[int64]tile) *tile {
-	for id, m := range t.sides {
-		if m.position == position {
-			result := tiles[id]
-			return &result
-		}
-	}
-	return nil
-}
-
-func draw(ascii [][]byte) {
-	for _, line := range ascii {
-		fmt.Println(line)
-	}
-}
-
-func part2(tiles map[int64]tile) {
-	var topLeft tile
-	for _, t := range tiles {
-		if len(t.sides) == 2 {
-			sides := make(map[string]int)
-			for _, m := range t.sides {
-				sides[m.position] = 1
-			}
-			if sides["right"] == 1 && sides["bottom"] == 1 {
-				topLeft = t
-				break
-			}
-		}
-	}
-
-	completePicture := [][]byte{}
-
-	current := &topLeft
-	// going down
-	for current != nil {
-		fragment := removeBorder(*current)
-		for _, line := range fragment {
-			completePicture = append(completePicture, line)
-		}
-		old := current
-		current = nextTile(*current, "bottom", tiles)
-		fmt.Println(old.id, current.id)
-	}
-
-	draw(completePicture)
+	fmt.Println(ids)
 }
